bb2c7b154e16d67d166626ceb19123da155c2e08
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-27 12:31:15 -0700

    Added constructors for DLList

--- DLList/DLList.java
+++ DLList/DLList.java
@@ -1,9 +1,9 @@
 // Doubly Linked List with prev and next pointers.
 public class DLList<T> {
     private class Node {
-        T value;
-        Node next;
-        Node prev;
+        public T value;
+        public Node next;
+        public Node prev;
 
         public Node(T v, Node n, Node p) {
             value = v;
@@ -13,5 +13,18 @@ public class DLList<T> {
     }
 
     private Node sentinel;
-    public int size
+    public int size;
+
+    // Constructor for empty DLList
+    public DLList() {
+        sentinel = new Node(null, null, null);
+        size = 0;
+    }
+
+    // Constructor for DLList
+    public DLList(T value) {
+        sentinel = new Node(null, null, null);
+        sentinel.next = new Node(value, sentinel, sentinel);
+        size = 1;
+    }
 }
