6e422cb09e59a0adcfe08c8b2b50c33772dea842
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-27 11:27:34 -0700

    Added a addLast method and created a LinkedList in main function

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -24,4 +24,44 @@ public class LinkedList<T> {
         sentinel = new Node(null, null);
         size = 0;
     }
+
+    // return the number of items in the Linked List
+    public int size() {
+        return size;
+    }
+
+    // Add an element to the start of the linked list
+    public void addFirst(T elem) {
+        sentinel.next = new Node(elem, sentinel.next);
+        size += 1;
+    }
+
+    // Get the first item in the linked list
+    public T getFirst() {
+        return sentinel.next.item;
+    }
+
+    // Add an item to the end of the LinkedList
+    public void addLast(T elem) {
+        // create a pointer to traverse through the linked list.
+        Node head = sentinel;
+
+        /*
+         * While the head pointer's next variable is not equal to a null value...
+         * advance the head pointer to the next link!
+         */
+        while (head.next != null) {
+            head = head.next;
+        }
+        // add the new item to the linked list
+        head.next = new Node(elem, null);
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> L = new LinkedList<Integer>();
+        L.addFirst(20);
+        L.addFirst(15);
+        L.addFirst(10);
+        System.out.println(L.getLast());
+    }
 }
