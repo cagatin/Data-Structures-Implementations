f35e3f16a56730c2efc7c5d990ba69eeb9498f7b
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-03 12:51:55 -0700

    Added the skeleton structure for ArrayList

---
+++ ArrayList/ArrayList.java
@@ -0,0 +1,25 @@
+public class ArrayList<T> {
+
+    // Constructor to create an empty ArrayList
+    public Arraylist() {}
+
+    // Inserts an item at the end of the ArrayList
+    public void addLast(T item) {
+    }
+
+    // Returns the item at the end of the ArrayList
+    public T getLast() {
+    }
+
+    // Retrieve the ith item in the ArrayList (Zero indexed)
+    public T get(int i) {
+    }
+
+    // Returns the number of items in the ArrayList
+    public int size() {
+    }
+
+    // Deletes the last item in the list and returns the deleted item
+    public T removeLast() {
+    }
+}
