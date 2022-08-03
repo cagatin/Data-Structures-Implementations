734c7584d93734d49dfb456024c7fcca98756894
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-03 12:57:30 -0700

    replaced generics with ints

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,25 +1,35 @@
-public class ArrayList<T> {
+public class ArrayList {
+    // Instance variables
+    private int[] items;
+    private int size;
 
     // Constructor to create an empty ArrayList
-    public Arraylist() {}
+    public Arraylist() {
+        items = new int[100];
+        size = 0;
+    }
 
     // Inserts an item at the end of the ArrayList
-    public void addLast(T item) {
+    public void addLast(int item) {
     }
 
     // Returns the item at the end of the ArrayList
-    public T getLast() {
+    public int getLast() {
+        return 0;
     }
 
     // Retrieve the ith item in the ArrayList (Zero indexed)
-    public T get(int i) {
+    public int get(int i) {
+        return 0;
     }
 
     // Returns the number of items in the ArrayList
     public int size() {
+        return 0;
     }
 
     // Deletes the last item in the list and returns the deleted item
-    public T removeLast() {
+    public int removeLast() {
+        return 0;
     }
 }
